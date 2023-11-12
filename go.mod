3dbadf2f5794bfb3eba6363ea3df4b62ba914399
poetofcode <cyber-post@yandex.ru>
2023-11-12 17:29:55 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go.mod
+
+go 1.21.0
+
+require github.com/mitchellh/go-ps v1.0.0
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	ps "github.com/mitchellh/go-ps"
+	"os/exec"
+	"time"
+	"strings"
+)
+
+func killQBitTorrent() {
+    app := "taskkill.exe"
+    arg0 := "/f"
+    arg1 := "/im"
+    arg2 := "qbittorrent.exe"
+
+    cmd := exec.Command(app, arg0, arg1, arg2)
+    stdout, err := cmd.Output()
+
+    if err != nil {
+        log.Println(err.Error())
+        return
+    }
+
+    // Print the output
+    log.Println(string(stdout))
+}
+
+func main() {
+	for {
+		processList, err := ps.Processes()
+	    if err != nil {
+	        log.Println("ps.Processes() Failed, are you using windows?")
+	        return
+	    }
+
+	    foundOutline := false
+	    foundTorrent := false
+
+	    for x := range processList {
+	        var process ps.Process
+	        process = processList[x]
+	        log.Printf("%d\t%s\n", process.Pid(), process.Executable())
+
+	        if strings.EqualFold(process.Executable(), "outline.exe") {
+	        	foundOutline = true
+	        }
+	        if strings.EqualFold(process.Executable(), "qbittorrent.exe") {
+	        	foundTorrent = true
+	        }
+	    }
+
+	    if foundOutline && foundTorrent {
+	    	killQBitTorrent()
+	    	log.Println("QBitTorrent stopped")
+	    } else {
+	    	log.Println("Outline or QBitTorrent not running")
+	    }
+
+		time.Sleep(10 * time.Second) 
+	}}
